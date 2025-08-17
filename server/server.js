2606568ba643bd5a64a0d299eeb229ec4ec72b65
Bhargav <bhargavk1290@gmail.com>
2025-08-17 15:40:46 +0530

    second

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.use('/api/our-team-journey', ourTeamJourneyRoutes);
 db();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`); 
 });  
 
